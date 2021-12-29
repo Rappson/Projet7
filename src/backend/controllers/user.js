4a07b413ab12ec43acb0d1692234a4dbc6c18065
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2021-12-29 15:43:30 +0100

    amelioration de la connexion a la bd

--- src/backend/controllers/user.js
+++ src/backend/controllers/user.js
@@ -1,15 +1,10 @@
-const app = require('../app')
-const connection = app.connection;
-
 exports.signup = (req, res, next) => {
     console.log(req);
     console.log(req.body);
 }
 
-exports.login = connection.query(
-    'SELECT * FROM user',
-    function(err, result, fields){
-        console.log(result);
-    }
-)
+exports.login = (req, res, next) => {
+    console.log(req);
+    console.log(req.body);
+}
 
